567a9f79d2ba4aeefb017f5a9eb9e64c4a5ea9cb
ikalangita <rahajason@gmail.com>
2024-08-29 00:29:06 +0200

    add opacity to deleted item

--- app/(protected)/admin/quiz/liste/page.tsx
+++ app/(protected)/admin/quiz/liste/page.tsx
@@ -2,7 +2,7 @@
 
 import { deleteQuiz, getQuizById, getQuizs } from "@/actions/quiz-admin/quiz";
 import { Quiz } from "@prisma/client";
-import { useState, useEffect, useTransition } from "react";
+import { useState, useEffect, useTransition, useRef, useCallback, RefObject } from "react";
 
 import Loading from "@/components/loading";
 
@@ -28,7 +28,8 @@ import { Button } from "@/components/ui/button";
 function ListeQuiz() {
   const [quizs, setQuizs] = useState<Quiz[] | undefined>(undefined);
   const [isPending, startTransition] = useTransition();
-  const [isPendingDeleted, startDeleteTransition] = useTransition()
+
+  const rowRefs = useRef<Map<string, HTMLTableRowElement | null>>(new Map());
 
   useEffect(() => {
     startTransition(() => {
@@ -43,24 +44,52 @@ function ListeQuiz() {
     });
   }, []);
 
+const setRowRef = useCallback((id: string) => (el: HTMLTableRowElement | null) => {
+  if (el) {
+    rowRefs.current.set(id, el);
+  } else {
+    rowRefs.current.delete(id);
+  }
+}, []);
+
   const handleDeleteQuiz = async (id: string) => {
 
-    if(quizs) {
+    const row = rowRefs.current.get(id);
+    console.log(row)
+      if (row) {
+        row.style.opacity = row.style.opacity === '0.5' ? '1' : '0.5';
+        row.style.pointerEvents = 'none';
+      }
 
-      startDeleteTransition(() => {
+
+
+
+      if(quizs) {
         deleteQuiz(id)
-        .then(response => {
-          if(response.error) {
-            toast(response.error);
-          } else {
-            const newQuizs = [...quizs];
-            const filteredQuizs = newQuizs.filter((quiz) => quiz.id !== id);
-            setQuizs(filteredQuizs);
-            toast(response.success);
-          }
-        })
-      })
-    }
+          .then(response => {
+            if(response.error) {
+              toast(response.error);
+              // Reset opacity if there's an error
+              if (row) {
+                row.style.opacity = '1';
+                row.style.pointerEvents = 'auto';
+              }
+            } else {
+              const newQuizs = [...quizs];
+              rowRefs.current.delete(id);
+              // Reset opacity if there's an error
+              if (row) {
+                row.style.opacity = '1';
+                row.style.pointerEvents = 'auto';
+              }
+              
+              setQuizs(() => {
+                return newQuizs.filter(quiz => quiz.id !== id)
+              });
+              toast(response.success);
+            }
+          })
+      }
   }
 
   return (
@@ -77,6 +106,7 @@ function ListeQuiz() {
       {quizs && (
         <>
           <Table>
+            {quizs.length === 0 && <TableCaption>Aucun quiz crée pour le moment</TableCaption>}
             <TableHeader>
               <TableRow>
                 <TableHead>Nom</TableHead>
@@ -87,7 +117,7 @@ function ListeQuiz() {
             <TableBody >
               {quizs.map((quiz, index) => {
                 return (
-                  <TableRow key={index}>
+                  <TableRow key={index} ref={setRowRef(quiz.id)} >
                     <TableCell>
                       <h1>
                         <Link
