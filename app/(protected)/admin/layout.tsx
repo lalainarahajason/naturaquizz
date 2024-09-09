d12b8820d50190d838d119f8b30c9afdac696053
ikalangita <rahajason@gmail.com>
2024-09-09 11:45:07 +0200

    split list quiz

--- app/(protected)/admin/layout.tsx
+++ app/(protected)/admin/layout.tsx
@@ -1,15 +1,12 @@
 "use server";
 
 import AdminSidebar from "@/components/sidebar";
-import Loading from "@/components/loading";
-import { Suspense } from "react";
 
 function layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="bg-white w-full max-w-[1024px] relative">
       <AdminSidebar />
-
-      <Suspense fallback={<Loading />}>{children}</Suspense>
+      {children}
     </div>
   );
 }
