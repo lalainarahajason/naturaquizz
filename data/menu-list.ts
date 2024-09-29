6ba345c715cb11e979b42ec308ee22813968b83e
ikalangita <rahajason@gmail.com>
2024-09-29 15:56:34 +0200

    remove user menu

--- data/menu-list.ts
+++ data/menu-list.ts
@@ -32,77 +32,7 @@ import {
   };
   
   export function getUserMenuList(pathname: string): Group[] {
-    return [
-      {
-        groupLabel: "",
-        menus: [
-          {
-            href: "/dashboard",
-            label: "Dashboard",
-            active: pathname.includes("/dashboard"),
-            icon: LayoutGrid,
-            submenus: []
-          }
-        ]
-      },
-      {
-        groupLabel: "Contents",
-        menus: [
-          {
-            href: "",
-            label: "Posts",
-            active: pathname.includes("/posts"),
-            icon: SquarePen,
-            submenus: [
-              {
-                href: "/posts",
-                label: "All Posts",
-                active: pathname === "/posts"
-              },
-              {
-                href: "/posts/new",
-                label: "New Post",
-                active: pathname === "/posts/new"
-              }
-            ]
-          },
-          {
-            href: "/categories",
-            label: "Categories",
-            active: pathname.includes("/categories"),
-            icon: Bookmark,
-            submenus: []
-          },
-          {
-            href: "/tags",
-            label: "Tags",
-            active: pathname.includes("/tags"),
-            icon: Tag,
-            submenus: []
-          }
-        ]
-      },
-      {
-        groupLabel: "Settings",
-        menus: [
-          
-          {
-            href: "/informations",
-            label: "Informations",
-            active: pathname.includes("/informations"),
-            icon: Settings,
-            submenus: []
-          },
-          {
-            href: "/settings",
-            label: "Account",
-            active: pathname.includes("/settings"),
-            icon: Users,
-            submenus: []
-          },
-        ]
-      }
-    ];
+    return [];
   }
 
   export function getAdminMenuList(pathname: string): Group[] {
