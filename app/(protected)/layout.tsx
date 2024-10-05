f55706e6fc312f89ad386550066119a36525c70b
ikalangita <rahajason@gmail.com>
2024-10-05 19:02:32 +0200

    fix user dashboard

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,16 +1,28 @@
 
 import { NavBar } from "./settings/_components/navbar";
 import AdminSidebar from "@/components/sidebar";
+import { CurrentRole } from "@/lib/auth";
 
-function ProtectedLayout({children}:{children: React.ReactNode}) {
+async function ProtectedLayout({children}:{children: React.ReactNode}) {
   
-  return (
-    <div className='w-full h-full flex flex-col gap-y-10 items-center '>
-        <NavBar />
-        <AdminSidebar />
-        {children}
-    </div>
-  )
+  const role = await CurrentRole();
+
+  if("ADMIN" === role ) {
+    return (
+      <div className='w-full h-full flex flex-col gap-y-10 items-center '>
+          <NavBar />
+          <AdminSidebar />
+          {children}
+      </div>
+      )
+  } else {
+    return (
+      <div className='w-full h-full flex flex-col gap-y-10 items-center '>
+          <NavBar />
+          {children}
+      </div>
+    )
+  }
 }
 
 export default ProtectedLayout
